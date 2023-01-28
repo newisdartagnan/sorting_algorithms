5a53a84765530a751c83e9b2498c4c557f3cc5ad
newisdartagnan <newisboris@gmail.com>
2023-01-27 19:11:10 -0800

    verified

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,38 @@
 #include "sort.h"
 /**
- * bubble_sort - sort array lements from min to max value
+ * bubble_sort - sort array elements from min to max value
  * @array: array
  * @size: array size
  */
 void bubble_sort(int *array, size_t size)
 {
 
-	size_t i, index, tmp = 0;
+	int i, n, tmp;
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+	else
+	{
+		if (!array || size == 0)
+		return;
+		tmp = 0;
+		n = size - 1;
+		while (n)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			for (i = 0; i < n; i++)
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				tmp = array[i];
+				if (tmp > array[i + 1])
+				{
+					array[i] = array[i + 1];
+					array[i + 1] = tmp;
+					print_array(array, size);
+				}
+
 			}
+
+			n -= 1;
 		}
+	}
+
 }
